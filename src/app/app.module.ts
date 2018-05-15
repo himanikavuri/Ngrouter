ac8bc8ef0a8b73e71d505bec8dd007eacddd0998
Himani Aravind <himaniaravindkavuri@gmail.com>
2018-05-15 23:13:23 +0530

    routeroutletcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule, RouterLinkActive } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+export const routes: Routes = [
+  { path: 'dash',component: DashboardComponent },
+  { path: '', component: LoginComponent },
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
